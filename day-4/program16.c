3c7e47283ae30e4eb97e092c71cf14f24d3b7401
Richa Dubey <dubeyricha857@gmail.com>
2026-06-07 13:20:11 +0530

    day-4 uploaded

---
+++ day-4/program16.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int start, end, num, temp, remainder, digits;
+    double sum;
+
+    printf("Enter start and end of range: ");
+    scanf("%d %d", &start, &end);
+
+    for (num = start; num <= end; num++) {
+        temp = num;
+        digits = 0;
+        sum = 0;
+
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        temp = num;
+
+        while (temp != 0) {
+            remainder = temp % 10;
+            sum += pow(remainder, digits);
+            temp /= 10;
+        }
+
+        if ((int)sum == num)
+            printf("%d ", num);
+    }
+
+    return 0;
+}
