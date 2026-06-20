7689aa48de0401c6bef75f0f63dfad7622c3d28a
Richa Dubey <dubeyricha857@gmail.com>
2026-06-20 23:58:55 +0530

    day-20 uploaded

---
+++ day-20/program79.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int r, c, i, j, sum;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    int A[r][c];
+
+    printf("Enter matrix elements:\n");
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            scanf("%d",&A[i][j]);
+
+    for(i=0;i<r;i++) {
+        sum = 0;
+        for(j=0;j<c;j++)
+            sum += A[i][j];
+
+        printf("Sum of Row %d = %d\n", i+1, sum);
+    }
+
+    return 0;
+}
